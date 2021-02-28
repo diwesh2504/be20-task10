df560d2a7e4b55011bdfa182d1f2cc16218a60e3
diwesh2504 <66382963+diwesh2504@users.noreply.github.com>
2021-02-28 10:09:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+var h2=document.querySelector('h2');
+var c=10;
+setTimeout(()=>{
+    h2.innerText=`${c--}`;
+     setTimeout(()=>{
+        h2.innerText=`${c--}`;
+        setTimeout(()=>{
+            h2.innerText=`${c--}`;
+            setTimeout(()=>{
+                h2.innerText=`${c--}`;
+                setTimeout(()=>{
+                    h2.innerText=`${c--}`;
+                    setTimeout(()=>{
+                        h2.innerText=`${c--}`;
+                        setTimeout(()=>{
+                            h2.innerText=`${c--}`;
+                            setTimeout(()=>{
+                                h2.innerText=`${c--}`;
+                                setTimeout(()=>{
+                                    h2.innerText=`${c--}`;
+                                    setTimeout(()=>{
+                                        h2.innerText=`${c--}`;
+                                        setTimeout(()=>{
+                                            h2.innerText=`Happy Birthday!`;
+                                        },1000)
+                                    },1000)
+                                },1000)
+                            },1000)
+                        },1000)
+                    },1000)
+                },1000)
+            },1000)
+        },1000)
+    },1000)
+    
+},1000)
